813493318272bdbfd8d9ef1c147d87bf8b295967
jinwoo-22 <botcommunity683@gmail.com>
2025-06-04 12:42:00 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,30 +1,48 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
-module.exports = {
-config: {
-name: "Out",
-aliases: ["l"],
-version: "1.0",
-author: "Sandy",
-countDown: 5,
-role: 2,
-shortDescription: "bot will leave gc",
-longDescription: "",
-category: "admin",
-guide: {
-vi: "{pn} [tid,blank]",
-en: "{pn} [tid,blank]"
-}
-},
 
-onStart: async function ({ api,event,args, message }) {
-var id;
-if (!args.join(" ")) {
-id = event.threadID;
-} else {
-id = parseInt(args.join(" "));
-}
-return api.sendMessage('▣JINWOO  LEAVE💢 :\n》SOJIB BOSS NE BOLA TO JANA PARE GA BYE 😞🖤.\n\n➤𝗕𝗘𝗬 𝗟𝗘𝗦 𝗡𝗔𝗭𝗘𝗦', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-}
+module.exports = {
+	config: {
+		name: "Out",
+		aliases: ["l"],
+		version: "1.0",
+		author: "Sandy + Modified by Sojib",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot will leave gc",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api, event, args }) {
+		let id;
+		if (!args.join(" ")) {
+			id = event.threadID;
+		} else {
+			id = parseInt(args.join(" "));
+		}
+
+		const time = new Date().toLocaleTimeString('en-US', {
+			hour: '2-digit',
+			minute: '2-digit',
+			hour12: true,
+			timeZone: 'Asia/Dhaka'
+		});
+
+		const leaveMessage = 
+`🖤 𝗝 𝗜 𝗡 𝗪 𝗢 𝗢  𝗟𝗘𝗔𝗩𝗘
+
+🎀 Bye guys...
+
+🕘 Leave time: ${time}`;
+
+		return api.sendMessage(leaveMessage, id, () => {
+			api.removeUserFromGroup(api.getCurrentUserID(), id);
+		});
+	}
 }
