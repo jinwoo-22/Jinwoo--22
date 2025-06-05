e44d95d1b51c89028791744393859d2fd1d015a9
jinwoo-22 <botcommunity683@gmail.com>
2025-06-05 22:23:19 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,42 +1,59 @@
 const axios = require("axios");
+
 const fs = require("fs-extra");
+
 const request = require("request");
 
 module.exports = {
+
 	config: {
+
 		name: "Out",
+
 		aliases: ["l"],
-		version: "1.5",
-		author: "Sandy + Modified by Sojib",
+
+		version: "1.0",
+
+		author: "Sandy",
+
 		countDown: 5,
+
 		role: 2,
-		shortDescription: "Bot leaves group stylishly",
+
+		shortDescription: "bot will leave gc",
+
 		longDescription: "",
+
 		category: "admin",
+
 		guide: {
-			en: "{pn}"
+
+			vi: "{pn} [tid,blank]",
+
+			en: "{pn} [tid,blank]"
+
 		}
+
 	},
 
-	onStart: async function ({ api, event, args }) {
-		let id = args.length ? parseInt(args.join(" ")) : event.threadID;
 
-		const time = new Date().toLocaleTimeString('en-US', {
-			hour: '2-digit',
-			minute: '2-digit',
-			hour12: true,
-			timeZone: 'Asia/Dhaka'
-		});
 
-		const leaveMessage = 
-`🖤 𝗝 𝗜 𝗡 𝗪 𝗢 𝗢  𝗟𝗘𝗔𝗩𝗘
+	onStart: async function ({ api,event,args, message }) {
 
-🎀 Bye guys...
+ var id;
 
-🕘 Leave time: ${time}`;
+ if (!args.join(" ")) {
 
-		api.sendMessage(leaveMessage, id, () => {
-			api.removeUserFromGroup(api.getCurrentUserID(), id);
-		});
-	}
+ id = event.threadID;
+
+ } else {
+
+ id = parseInt(args.join(" "));
+
+ }
+
+ return api.sendMessage('▣ 𝗕𝗢𝗧 𝗟𝗘𝗔𝗩𝗘:\n》Mon succès est inévitable, car je suis destiné à être au sommet.\n\n➤𝗕𝗘𝗬 𝗟𝗘𝗦 𝗡𝗔𝗭𝗘𝗦', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+
+		}
+
 		}
